ee852bdcef3870952aac66fed8369bfac89bc563
hanswatkins <hanswatkins@gmail.com>
2022-07-01 19:02:41 -0700

    added menu for mobile that pops out, not functioning yet

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('connected to script.js');
+console.log('Connected to script.js');
 
 //query selectors
 homeLink = document.querySelector('#home-link')
@@ -6,22 +6,24 @@ aboutLink = document.querySelector('#about-link')
 projectsLink = document.querySelector('#projects-link')
 resumeLink = document.querySelector('#resume-link')
 contactLink = document.querySelector('#contact-link')
+hamburgerMenu = document.querySelector('#hamburger-menu')
 
 //functions
-switchPageHome = function () {
-    document.querySelector('.landing-page').style.display = 'block';
-    document.querySelector('.projects').style.display = 'none';
-    document.querySelector('.contact-me').style.display = 'none';
-    document.querySelector('.resume').style.display = 'none';
-    document.querySelector('.about-page').style.display = 'none';
-};
 
+//switch page function
 switchPageAbout = function () {
     document.querySelector('.landing-page').style.display = 'none';
     document.querySelector('.projects').style.display = 'none';
     document.querySelector('.contact-me').style.display = 'none';
     document.querySelector('.resume').style.display = 'none';
     document.querySelector('.about-page').style.display = 'block';
+};
+switchPageHome = function () {
+    document.querySelector('.landing-page').style.display = 'block';
+    document.querySelector('.projects').style.display = 'none';
+    document.querySelector('.contact-me').style.display = 'none';
+    document.querySelector('.resume').style.display = 'none';
+    document.querySelector('.about-page').style.display = 'none';
 };
 switchPageProjects = function () {
     document.querySelector('.landing-page').style.display = 'none';
@@ -45,11 +47,28 @@ switchPageContact = function () {
     document.querySelector('.about-page').style.display = 'none';
 };
 
+
+
+
+//button opens the menu
+//the function hides whatever page you're currently on
+
+function openMenu() {
+    let x = document.querySelector(".menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
 //event listeners
 aboutLink.addEventListener('click', switchPageAbout)
 homeLink.addEventListener('click', switchPageHome)
 projectsLink.addEventListener('click', switchPageProjects)
 resumeLink.addEventListener('click', switchPageResume)
 contactLink.addEventListener('click', switchPageContact)
+hamburgerMenu.addEventListener('click', openMenu)
 
 
